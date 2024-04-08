f240aebc4af6fd777b970dca7e5622404cd9559a
Deyvison Iury <iurydeyvison@gmail.com>
2024-04-08 14:53:38 -0300

    Style login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Button } from "@/components/ui/button";
 import { createClient } from "@/utils/supabase/client"
 
 export default function Auth() {
@@ -12,8 +13,9 @@ export default function Auth() {
     })
   }
   return (
-    <div className="h-screen">
-      <button onClick={() => SignInWithGoogle()}>Login</button>
+    <div className="h-screen flex flex-col justify-center items-center shrink">
+      <h1 className="font-bold text-9xl fixed mb-96">PumpTrack</h1>
+      <Button onClick={() => SignInWithGoogle()} className="text-2xl h-20" size="lg">Login with google</Button>
     </div>
   )
 }
